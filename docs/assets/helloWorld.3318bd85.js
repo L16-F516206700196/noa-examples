7a6da2850b2a005303dcf2783882fe1349f06ee7
L16-F516206700196 <l16f51620@gmail.com>
2026-07-16 20:24:44 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -1,1 +1,209 @@
-import{E as Engine}from"./index.2b3d1184.js";import{C as D}from"./babylon.39bd9ef3.js";var opts={debug:!0,showFPS:!0,chunkSize:32,chunkAddDistance:2.5,chunkRemoveDistance:3.5,texturePath:"textures/"},noa=new Engine(opts),e=noa;let seedNum=0,scale=16,heightScale=4,caveThreshold=.7,leniency=.066;const dot=(e,t)=>e[0]*t[0]+e[1]*t[1],dot3=(e,t)=>e[0]*t[0]+e[1]*t[1]+e[2]*t[2],fade=e=>6*e**5-15*e**4+10*e**3,lerp=(e,t,a)=>e+(t-e)*a,smoothstep=(e,t,a)=>{let l=Math.max(0,Math.min(1,(a-e)/(t-e)));return 6*l**5-15*l**4+10*l**3},rgba_hex=e=>{let t="#";for(i of e.map(e=>Math.floor(e))){let e=i.toString(16);e.length<2&&(e=`0${e}`),t=`${t}${e}`}return t},generateHash=e=>{let t=2166136261;for(let a=0;a<e.length;a++)t^=e[a].charCodeAt(),t=Math.imul(t,16777619);return t},randomS=e=>(e^=e<<13,e^=e>>>7,((e^=e<<17)>>>0)/4294967295);let cacheAG=new Map,cacheAG3=new Map;const angleGen=(e,t)=>{let a=`${e},${t}`;if(cacheAG.has(a))return cacheAG.get(a);let l=randomS(generateHash(`${e},${t}|${seedNum}`))*Math.PI*2,n=[Math.cos(l),Math.sin(l)];return cacheAG.set(a,n),n},angleGen3=(e,t,a)=>{let l=`${e},${t},${a}`;if(cacheAG3.has(l))return cacheAG3.get(l);let n=`${e},${t},${a}|${seedNum}`,r=randomS(generateHash(`${n}|a`)),h=randomS(generateHash(`${n}|b`));const o=r*Math.PI*2,c=Math.acos(1-2*h),s=Math.sin(c);let d=[Math.cos(o)*s,Math.cos(c),Math.sin(o)*s];return cacheAG3.set(l,d),d},perlin=(e,t)=>{let a=Math.floor(e),l=a+1,n=Math.floor(t),r=n+1;const h=angleGen(a,n),o=angleGen(l,n),c=angleGen(a,r),s=angleGen(l,r);let d=e-a,g=t-n,i=fade(d),G=fade(g),M=[d-1,g],p=[d,g-1],f=[d-1,g-1],$=dot(h,[d,g]),u=dot(o,M),m=dot(c,p),A=dot(s,f),S=lerp($,u,i),v=lerp(m,A,i);return lerp(S,v,G)},perlin3=(e,t,a)=>{let l=Math.floor(e),n=l+1,r=Math.floor(t),h=r+1,o=Math.floor(a),c=o+1;const s=angleGen3(l,r,o),d=angleGen3(n,r,o),g=angleGen3(l,h,o),i=angleGen3(n,h,o),G=angleGen3(l,r,c),M=angleGen3(n,r,c),p=angleGen3(l,h,c),f=angleGen3(n,h,c);let $=e-l,u=t-r,m=a-o,A=fade($),S=fade(u),v=fade(m),H=[$-1,u,m],P=[$,u-1,m],y=[$-1,u-1,m],B=[$,u,m-1],N=[$-1,u,m-1],T=[$,u-1,m-1],b=[$-1,u-1,m-1],w=dot3(s,[$,u,m]),x=dot3(d,H),C=dot3(g,P),F=dot3(i,y),I=dot3(G,B),W=dot3(M,N),_=dot3(p,T),j=dot3(f,b),k=lerp(w,x,A),q=lerp(C,F,A),z=lerp(I,W,A),D=lerp(_,j,A),E=lerp(k,q,S),J=lerp(z,D,S);return lerp(E,J,v)},evalPerlinWithFBM=(e,t,a)=>{let l=e/scale,n=t/scale,r=a/scale;return perlin3(l/16,n/24,r/16)*(scale/heightScale)/16+perlin3(l/8,n/12,r/8)*(scale/heightScale)/8+perlin3(l/4,n/6,r/4)*(scale/heightScale)/4},shouldBeCaveAir=(e,t,a)=>{let l=evalPerlinWithFBM(1*e,1*t,1*a);l+=7/8,l/=7/4;return smoothstep(caveThreshold-leniency,caveThreshold+leniency,l)>.67};var brownish=[.45,.36,.22,.5],greenish=[.1,.8,.2,.6];noa.registry.registerMaterial("dirt",{color:brownish}),noa.registry.registerMaterial("grass",{color:greenish}),noa.registry.registerMaterial("stone",{color:[.5,.5,.5,.5]}),noa.registry.registerMaterial("bedrock",{color:[.1,.1,.1]});var dirtID=noa.registry.registerBlock(1,{material:"dirt"}),grassID=noa.registry.registerBlock(2,{material:"grass"}),stoneID=noa.registry.registerBlock(3,{material:"stone"}),bedrockID=noa.registry.registerBlock(4,{material:"bedrock"});function getVoxelID(e,r,a){let t=e/scale,l=a/scale,n=perlin(t,l)*(scale/heightScale)+perlin(t/2,l/2)*(scale/heightScale)/2+perlin(t/4,l/4)*(scale/heightScale)/4+perlin(t/8,l/8)*(scale/heightScale)/8+perlin(t/16,l/16)*(scale/heightScale)/16,o=Math.round(n);return r<-99?0:-99===r?bedrockID:shouldBeCaveAir(e,r,a)?0:r<o-5?stoneID:r<o-1?dirtID:r<o?grassID:0}noa.world.on("worldDataNeeded",function(e,r,a,t,l){for(var n=0;n<r.shape[0];n++)for(var o=0;o<r.shape[1];o++)for(var s=0;s<r.shape[2];s++){var h=getVoxelID(a+n,t+o,l+s);r.set(n,o,s,h)}noa.world.setChunkData(e,r)});var g=e.playerEntity,m=e.entities.getPositionData(g),d=m.width,f=m.height,z=e.rendering.getScene(),a=D("player-mesh",{},z);a.scaling.x=d;a.scaling.z=d;a.scaling.y=f;a.material=e.rendering.makeStandardMaterial();e.entities.addComponent(g,e.entities.names.mesh,{mesh:a,offset:[0,f/2,0]});e.inputs.down.on("fire",function(){if(e.targetedBlock){var r=e.targetedBlock.position;e.setBlock(0,r[0],r[1],r[2])}});e.inputs.down.on("alt-fire",function(){if(e.targetedBlock){var r=e.targetedBlock.adjacent;e.setBlock(grassID,r[0],r[1],r[2])}});e.inputs.bind("alt-fire","KeyE");e.on("tick",function(r){var t=e.inputs.pointerState.scrolly;t!==0&&(e.camera.zoomDistance+=t>0?1:-1,e.camera.zoomDistance<0&&(e.camera.zoomDistance=0),e.camera.zoomDistance>10&&(e.camera.zoomDistance=10))});
+import{E as Engine}from"./index.2b3d1184.js";import{C as D}from"./babylon.39bd9ef3.js";var opts={debug:!0,showFPS:!0,chunkSize:32,chunkAddDistance:2.5,chunkRemoveDistance:3.5,texturePath:"textures/"},noa=new Engine(opts),e=noa;
+let seedNum = 0;
+let scale=16;
+let heightScale=4;
+let caveThreshold = 0.7, leniency = 0.066;
+
+const dot = (a,b) => (a[0]*b[0])+(a[1]*b[1]);
+const dot3 = (a,b) => (a[0]*b[0])+(a[1]*b[1])+(a[2]*b[2]);
+const fade = x => 6*(x**5) - 15*(x**4) + 10*(x**3);
+const lerp = (a, b, n) => a + ((b - a)*n);
+const smoothstep=(a,b,n)=>{
+	let t = Math.max(0,Math.min(1,(n-a)/(b-a)));
+	return 6*(t**5) - 15*(t**4) + 10*(t**3);
+}
+const rgba_hex=rgba=>{
+	let hex="#";
+    for(i of rgba.map(i=>Math.floor(i))){
+    	let checkedV=i.toString(16);
+        if(checkedV.length<2)checkedV=`0${checkedV}`;
+        hex=`${hex}${checkedV}`;
+    }
+    return hex;
+}
+
+const generateHash=str=>{
+    let hash=2166136261;
+    for(let i=0;i<str.length;i++){
+        hash^=(str[i].charCodeAt());
+        hash = Math.imul(hash,16777619);
+    }
+    
+    return hash;
+}
+
+const randomS=s=>{
+	s^=(s<<13);
+	s^=(s>>>7);
+    s^=(s<<17); 
+	return ((s>>>0)/4294967295)
+}
+//limit or cache per chunk later.
+let cacheAG=new Map();
+let cacheAG3=new Map();
+
+const angleGen = (x, y) => {
+	let keyCheck=`${x},${y}`;
+	if(cacheAG.has(keyCheck))return cacheAG.get(keyCheck);
+	let seedU = `${x},${y}|${seedNum}`;
+	let angle = randomS(generateHash(seedU))*Math.PI*2;
+	let rV=[Math.cos(angle),Math.sin(angle)];
+	cacheAG.set(keyCheck,rV);
+	return rV //for length 1
+}
+
+const angleGen3 = (x, y, z) => {
+	let keyCheck=`${x},${y},${z}`;
+	if(cacheAG3.has(keyCheck))return cacheAG3.get(keyCheck);
+	let seedU = `${x},${y},${z}|${seedNum}`;
+	let r1 = randomS(generateHash(`${seedU}|a`));
+	let r2 = randomS(generateHash(`${seedU}|b`));
+
+	//ty for the normalisation chatgpt
+	const theta = r1 * Math.PI * 2;           // 0..2pi
+	const phi = Math.acos(1 - 2 * r2);       // 0..pi (gives uniform-ish directions)
+
+	const sinPhi = Math.sin(phi);
+	let rV=[Math.cos(theta) * sinPhi, Math.cos(phi), Math.sin(theta) * sinPhi];
+	cacheAG3.set(keyCheck,rV);
+	return rV;
+}
+
+const perlin = (x, y) => {
+	
+    let x_0 = Math.floor(x), x_1 = x_0+1, y_0 = Math.floor(y), y_1 = y_0+1; 
+	const g_00=angleGen(x_0,y_0);
+	const g_10=angleGen(x_1,y_0);
+	const g_01=angleGen(x_0,y_1);
+	const g_11=angleGen(x_1,y_1);
+    let frx=x-x_0;
+    let fry=y-y_0;
+    let u = fade(frx), v = fade(fry); 
+    let d_00 = [frx, fry], d_10 = [frx-1, fry], d_01 = [frx, fry-1], d_11 = [frx-1, fry-1]; 
+    let s_00 = dot(g_00,d_00), s_10 = dot(g_10,d_10), s_01 = dot(g_01,d_01), s_11 = dot(g_11,d_11); 
+    let lx0 = lerp(s_00,s_10,u), lx1 = lerp(s_01,s_11,u); 
+    let value = lerp(lx0,lx1,v);
+    return value;
+}
+
+const perlin3 = (x, y, z) => {
+	
+    let x_0 = Math.floor(x), x_1 = x_0+1, y_0 = Math.floor(y), y_1 = y_0+1, z_0 = Math.floor(z), z_1 = z_0+1; 
+	const g_000=angleGen3(x_0,y_0,z_0);
+	const g_100=angleGen3(x_1,y_0,z_0);
+	const g_010=angleGen3(x_0,y_1,z_0);
+	const g_110=angleGen3(x_1,y_1,z_0);
+
+	const g_001=angleGen3(x_0,y_0,z_1);
+	const g_101=angleGen3(x_1,y_0,z_1);
+	const g_011=angleGen3(x_0,y_1,z_1);
+	const g_111=angleGen3(x_1,y_1,z_1);
+    let frx=x-x_0;
+    let fry=y-y_0;
+	let frz=z-z_0;
+    let u = fade(frx), v = fade(fry), w = fade(frz);
+    let d_000 = [frx, fry, frz], d_100 = [frx-1, fry, frz], d_010 = [frx, fry-1, frz], d_110 = [frx-1, fry-1, frz];
+	let d_001 = [frx, fry, frz-1], d_101 = [frx-1, fry, frz-1], d_011 = [frx, fry-1, frz-1], d_111 = [frx-1, fry-1, frz-1]; 
+    let s_000 = dot3(g_000,d_000), s_100 = dot3(g_100,d_100), s_010 = dot3(g_010,d_010), s_110 = dot3(g_110,d_110); 
+	let s_001 = dot3(g_001,d_001), s_101 = dot3(g_101,d_101), s_011 = dot3(g_011,d_011), s_111 = dot3(g_111,d_111); 
+    let lx0 = lerp(s_000,s_100,u), lx1 = lerp(s_010,s_110,u); 
+	let lx2 = lerp(s_001,s_101,u), lx3 = lerp(s_011,s_111,u); 
+	let ly0 = lerp(lx0,lx1,v), ly1 = lerp(lx2,lx3,v);
+    let value = lerp(ly0,ly1,w);
+    return value;
+}
+//perlin frequencies here may look off because normally you should multiply x/16,y/16 by a higher number. However, k and l, which are used, are actually x/scale and z/scale, so it's the opposite here. Sorry for the strange behaviour lol
+const evalPerlinWithFBM=(x,y,z)=>{
+	let k=x/scale,l=y/scale,m=z/scale;
+	return (perlin3(k/16,l/24,m/16)*(scale/heightScale)/16)
+	+(perlin3(k/ 8,l/ 12,m/ 8)*(scale/heightScale)/8)
+	+(perlin3(k/ 3,l/ 4.5,m/ 3)*(scale/heightScale)/4)
+	+(perlin3(k/ 1,l/ 1.5,m/ 1)*(scale/heightScale)/4)
+	//three octaves for now because five is super expensive.
+}
+const shouldBeCaveAir = (x, y, z) => {
+	const sx=1,sy=1,sz=1;
+	let cV=evalPerlinWithFBM(x*sx,y*sy,z*sz);
+	cV+=7/8
+	cV/=7/4;
+	const t=smoothstep(caveThreshold-leniency,caveThreshold+leniency,cV)
+	return t>0.67;
+}
+
+/*
+ *
+ *      Registering voxel types
+ * 
+ *  Two step process. First you register a material, specifying the 
+ *  color/texture/etc. of a given block face, then you register a 
+ *  block, which specifies the materials for a given block type.
+ * 
+*/
+
+// block materials (just colors for this demo)
+var brownish = [0.45, 0.36, 0.22, 0.5]
+var greenish = [0.1, 0.8, 0.2, 0.6]
+noa.registry.registerMaterial('dirt', {color:brownish});
+noa.registry.registerMaterial('grass', {color:greenish});
+noa.registry.registerMaterial('stone', {color:[0.5,0.5,0.5,0.5]}); //stone
+noa.registry.registerMaterial('bedrock', {color:[0.1,0.1,0.1],});
+//noa.registry.registerMaterial(name, color, textureURL)
+
+// block types and their material names
+var dirtID = noa.registry.registerBlock(1, { material: 'dirt' })
+var grassID = noa.registry.registerBlock(2, { material: 'grass' })
+var stoneID = noa.registry.registerBlock(3, {material: 'stone'})
+var bedrockID = noa.registry.registerBlock(4, {material: 'bedrock'})
+//noa.registry.registerMaterial(blockID, opts)
+
+
+
+/*
+ * 
+ *      World generation
+ * 
+ *  The world is divided into chunks, and `noa` will emit an 
+ *  `worldDataNeeded` event for each chunk of data it needs.
+ *  The game client should catch this, and call 
+ *  `noa.world.setChunkData` whenever the world data is ready.
+ *  (The latter can be done asynchronously.)
+ * 
+*/
+
+// simple height map worldgen function
+function getVoxelID(x, y, z) {
+	let k=x/scale;
+	let l=z/scale;
+	let height=(perlin(k,l)*(scale/heightScale))
+	+(perlin(k/2,l/2)*(scale/heightScale)/2)
+	+(perlin(k/4,l/4)*(scale/heightScale)/4)
+	+(perlin(k/8,l/8)*(scale/heightScale)/8)
+	+(perlin(k/16,l/16)*(scale/heightScale)/16);
+	let amount = Math.round(height);
+	if (y < -99) return 0;
+	if (y === -99) return bedrockID
+	if(shouldBeCaveAir(x,y,z))return 0;
+	if (y < amount-5)return stoneID
+    if (y < amount-1) return dirtID
+    
+    if (y < amount) return grassID
+    return 0 // signifying empty space
+}
+
+// register for world events
+noa.world.on('worldDataNeeded', function (id, data, x, y, z) {
+    // `id` - a unique string id for the chunk
+    // `data` - an `ndarray` of voxel ID data (see: https://github.com/scijs/ndarray)
+    // `x, y, z` - world coords of the corner of the chunk
+    for (var i = 0; i < data.shape[0]; i++) {
+        for (var j = 0; j < data.shape[1]; j++) {
+            for (var k = 0; k < data.shape[2]; k++) {
+                var voxelID = getVoxelID(x + i, y + j, z + k)
+                data.set(i, j, k, voxelID)
+            }
+        }
+    }
+    // tell noa the chunk's terrain data is now set
+    noa.world.setChunkData(id, data)
+});
+var g=e.playerEntity,m=e.entities.getPositionData(g),d=m.width,f=m.height,z=e.rendering.getScene(),a=D("player-mesh",{},z);a.scaling.x=d;a.scaling.z=d;a.scaling.y=f;a.material=e.rendering.makeStandardMaterial();e.entities.addComponent(g,e.entities.names.mesh,{mesh:a,offset:[0,f/2,0]});e.inputs.down.on("fire",function(){if(e.targetedBlock){var r=e.targetedBlock.position;e.setBlock(0,r[0],r[1],r[2])}});e.inputs.down.on("alt-fire",function(){if(e.targetedBlock){var r=e.targetedBlock.adjacent;e.setBlock(grassID,r[0],r[1],r[2])}});e.inputs.bind("alt-fire","KeyE");e.on("tick",function(r){var t=e.inputs.pointerState.scrolly;t!==0&&(e.camera.zoomDistance+=t>0?1:-1,e.camera.zoomDistance<0&&(e.camera.zoomDistance=0),e.camera.zoomDistance>10&&(e.camera.zoomDistance=10))});
