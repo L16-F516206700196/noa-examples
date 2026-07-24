43f516ada7f240460a8fbed451d40a067ac75925
L16-F516206700196 <l16f51620@gmail.com>
2026-07-23 20:40:03 -0700

    Update helloWorld.3318bd85.js

--- docs/assets/helloWorld.3318bd85.js
+++ docs/assets/helloWorld.3318bd85.js
@@ -682,8 +682,8 @@ noa.inputs.down.on("toggle-check-place",()=>{
 // each tick, consume any scroll events and use them to zoom camera
 noa.on('tick', function (dt) {
 	let vel=fm.velocity,speed=Math.sqrt(vel[0]**2 + vel[2]**2);
-	let displayCoords=noa.entities.getPosition(g).map(i=>`<br>${Math.round(i*1e4)/1e4)}`);
-	let displayVel=fm.velocity.map(i => `<br> ${Math.round(i*1e4)/1e4)} `)
+	let displayCoords=noa.entities.getPosition(g).map(i=>`<br>${Math.round(i*1e4)/1e4}`);
+	let displayVel=fm.velocity.map(i => `<br> ${Math.round(i*1e4)/1e4} `)
 	document.getElementById("debug-info").innerHTML=`
 	Coordinates: X/Y/Z ${displayCoords}
 	<br>Vel: X/Y/Z ${displayVel}<br>Speed: ${speed} b/s`;
